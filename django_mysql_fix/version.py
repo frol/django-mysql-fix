bfa586d23543bbe79a26ab8c0665f478e554745a
frol <frolvlad@gmail.com>
2014-04-16 00:27:29 +0100

    Fixed package name; Added long description

--- django_mysql_fix/version.py
+++ django_mysql_fix/version.py
@@ -1,3 +1,3 @@
-VERSION = (0, 1)
+VERSION = (0, 1, 1)
 
 __version__ = '.'.join(unicode(x) for x in VERSION)
--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup, find_packages
 
 from django_mysql_fix.version import __version__
 
+with open('README.md') as long_description_file:
+    long_description = long_description_file.read()
 
-setup(name='django_mysql_fix',
+setup(name='django-mysql-fix',
     version=__version__,
     description="This project contains optimizations (hacks) for MySQL for Django ORM",
+    long_description=long_description,
     author="Vladyslav Frolov",
     author_email="frolvlad@gmail.com",
-    url='http://prostoksi.com',
+    url='https://github.com/frol/django-mysql-fix',
     packages=find_packages(),
     install_requires=('django', ),
 )
